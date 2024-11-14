cbf70fb1bcabf134a04bf907724803c0a19c3383
Abhay Mishra <techabhay06@gmail.com>
2024-11-14 17:27:22 +0530

    Add new commit

--- Guess-Perfect-Number-game2/main.py
+++ Guess-Perfect-Number-game2/main.py
@@ -1,2 +1,23 @@
 
 # Here we write code for build Guess-Perfect-Number game in Python language-
+
+import random
+
+n = random.randint(1, 100)
+a = -1
+guesnum = 1
+
+while (a != n):
+
+    a = int(input("Guess the number: "))
+
+    if (a>n):
+        print("select lower number")
+        guesnum += 1
+
+    elif (a<n):
+        print("select higher number")
+        guesnum += 1
+
+print(f"Hurry 🤩, You guess it the number {n} correct in {guesnum} attempts.")
+print("Congratulation🥳", "Let's play once again!")
